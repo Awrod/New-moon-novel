cfc8fe4a50ba5824acf5f9b4ee1ab0caefbaaccd
longw <532934252@qq.com>
2021-03-08 19:34:44 +0800

    初始化

---
+++ src/test/java/com/newmoonnovel/novel/NovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newmoonnovel.novel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
